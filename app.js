c2fccb078a8fcd50dfe96f5f7a65e15ecb54e2b1
Exequiel <exee.lp@hotmail.com>
2025-02-04 01:52:51 -0300

    Agregamos algoritmos para correcto funcionamiento.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo(){
+    let friend=document.getElementById("amigo").value;
+    if(friend==null || friend.trim()==""){
+        alert("insertar un nombre, esta vacio");
+        return;
+    }
+
+    let amigoInsertado = document.createElement("li");
+    amigoInsertado.textContent = friend;
+    document.getElementById("listaAmigos").appendChild(amigoInsertado);
+    amigos.push(friend);
+    document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo(){
+if(amigos.length<=0){
+    alert("agrega amigos para sortear");
+    return;
+}
+let numeroSorteo=Math.floor(Math.random()*amigos.length);
+let amigoSorteado = amigos[numeroSorteo];
+document.getElementById("resultado").innerText=amigoSorteado;
+
+}
